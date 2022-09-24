152576311137e027f6ac6a81681e9249f38c4f90
Krishrx <114279261+Krishrx@users.noreply.github.com>
2022-09-24 12:35:50 +0530

    Update Trial.java

--- Trial.java
+++ Trial.java
@@ -1,6 +1,7 @@
 public class Trial {
     public static void main(String[] args){
         System.out.println(add(5,5));
+         System.out.println(add(5,5));
     }
     static int add(int a,int b){
         return a+b;

d023c323c6975080157d7738cd62b21882cbc9a0
Krishrx <114279261+Krishrx@users.noreply.github.com>
2022-09-24 12:47:49 +0530

    Update Trial.java

--- Trial.java
+++ Trial.java
@@ -1,9 +1,12 @@
 public class Trial {
     public static void main(String[] args){
         System.out.println(add(5,5));
-         System.out.println(add(5,5));
+         System.out.println(mul(5,5));
     }
     static int add(int a,int b){
         return a+b;
+    }
+      static int mul(int a,int b){
+        return a*b;
     }
 }
